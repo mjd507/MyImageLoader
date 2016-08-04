156ea7bee0144ac16771c56c9a9fdc26860884f2
majd <836240219@qq.com>
2016-08-04 19:00:17 +0800

    little change

--- app/src/main/java/com/fighting/myimageloader/loader/AbsLoader.java
+++ app/src/main/java/com/fighting/myimageloader/loader/AbsLoader.java
@@ -1,5 +1,10 @@
 package com.fighting.myimageloader.loader;
 
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import com.fighting.myimageloader.cache.BitmapCache;
+import com.fighting.myimageloader.core.MyImageLoader;
 import com.fighting.myimageloader.request.BitmapRequest;
 
 /**
@@ -7,9 +12,39 @@ import com.fighting.myimageloader.request.BitmapRequest;
  * Created by MaJD on 2016/8/4.
  */
 
-public class AbsLoader implements Loader {
+public abstract class AbsLoader implements Loader {
+
+    private static BitmapCache mCache = MyImageLoader.getInstance().getConfig().bitmapCache;
+
     @Override
     public void loadImage(BitmapRequest request) {
+        Bitmap resultBitmap = mCache.get(request);
+        Log.e("", "### 是否有缓存 : " + resultBitmap + ", uri = " + request.imageUri);
+        if (resultBitmap == null) {
+            showLoading(request);
+            resultBitmap = onLoadImage(request);
+            cacheBitmap(request, resultBitmap);
+        } else {
+            request.justCacheInMem = true;
+        }
+
+        deliverToUIThread(request, resultBitmap);
 
     }
+    protected void showLoading(BitmapRequest request){
+
+    }
+    protected abstract Bitmap onLoadImage(BitmapRequest request);
+
+    protected void cacheBitmap(BitmapRequest request, Bitmap bitmap){
+
+    }
+    protected void deliverToUIThread(BitmapRequest request, Bitmap resultBitmap){
+
+    }
+
+
+
+
+
 }
