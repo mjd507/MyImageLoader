4bdd6c5cbada11f97221c824ad1ebf7a97901eba
majd <836240219@qq.com>
2016-08-08 16:34:18 +0800

    完善加载显示

--- app/src/main/java/com/fighting/myimageloader/cache/NoCache.java
+++ app/src/main/java/com/fighting/myimageloader/cache/NoCache.java
@@ -1,9 +1,27 @@
 package com.fighting.myimageloader.cache;
 
+import android.graphics.Bitmap;
+
+import com.fighting.myimageloader.request.BitmapRequest;
+
 /**
  * 描述：
  * Created by MaJD on 2016/8/4.
  */
 
 public class NoCache implements BitmapCache {
+    @Override
+    public Bitmap get(BitmapRequest request) {
+        return null;
+    }
+
+    @Override
+    public void put(BitmapRequest key, Bitmap bitmap) {
+
+    }
+
+    @Override
+    public void remove(BitmapRequest key) {
+
+    }
 }
